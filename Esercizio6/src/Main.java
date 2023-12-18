d32100d6f196f3e5077be1218d49648f9121d0f4
bananapilota <fabiopetrosillo2@gmail.com>
2023-12-18 09:52:00 +0100

    modificato esercizio 7 per implementare anche il reattangolo nel main

--- Esercizio6/src/Main.java
+++ Esercizio6/src/Main.java
@@ -2,5 +2,7 @@ public class Main {
   public static void main(String[] args) {
     Triangle triangle = new Triangle(10, 9);
     System.out.println(triangle.getArea());
+    Rectangle rectangle = new Rectangle(10, 5);
+    System.out.println(rectangle.getArea());
   }
 }
