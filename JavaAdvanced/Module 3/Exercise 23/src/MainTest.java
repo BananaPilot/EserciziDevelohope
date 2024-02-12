19ea224bffbba30dc2c68c2ffdbfa96009062908
BananaPilot <fabiopetrosillo2@gmail.com>
2024-02-12 21:34:03 +0100

    fixed m4e23

--- JavaAdvanced/Module 3/Exercise 23/src/MainTest.java
+++ JavaAdvanced/Module 3/Exercise 23/src/MainTest.java
@@ -1,20 +1,23 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
+import static org.junit.Assert.*;
+
 public class MainTest {
 
-  @Test
-  public void main() {
-    OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-    System.out.println(date.format(DateTimeFormatter.ofPattern("DD mmmm YYYY").localizedBy(Locale.ITALIAN)));
-  }
+    @Test(expected = IllegalArgumentException.class)
+    public void main() {
+        OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        System.out.println(date.format(DateTimeFormatter.ofPattern("DD mmmm YYYY").localizedBy(Locale.ITALIAN)));
+    }
 
-  @Test
-  public void main2() {
-    OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-    System.out.println(date.format(DateTimeFormatter.ofPattern("dd MM YYYY").localizedBy(Locale.ITALIAN)));
-  }
+    @Test
+    public void main2() {
+        OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        System.out.println(date.format(DateTimeFormatter.ofPattern("dd MM YYYY").localizedBy(Locale.ITALIAN)));
+        assertEquals("01 03 2023", date.format(DateTimeFormatter.ofPattern("dd MM YYYY").localizedBy(Locale.ITALIAN)));
+    }
 }
