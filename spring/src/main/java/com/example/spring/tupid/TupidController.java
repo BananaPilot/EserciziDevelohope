8dc5d95260f39e97f83bdd3c0c0b7cb5abc6e15e
= <=>
2024-03-20 14:59:37 +0100

    m2e1

--- spring/src/main/java/com/example/spring/tupid/TupidController.java
+++ spring/src/main/java/com/example/spring/tupid/TupidController.java
@@ -2,12 +2,19 @@ package com.example.spring.tupid;
 
 import jakarta.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TupidController {
-    @GetMapping("/")
-    public String get(@PathParam("name") String name, @PathParam("region") String region) {
-        return "Hi " + name + ", how is the weather in " + region + "?";
+    @GetMapping("/{name}")
+    public String getName(@PathVariable("name") String name) {
+        return "Hi " + name;
+    }
+
+    @PostMapping("/{name}")
+    public String reverseName(@PathVariable("name") String name) {
+        return new StringBuilder(name).reverse().toString();
     }
 }
