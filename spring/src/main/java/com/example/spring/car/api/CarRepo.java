d1ccf16964bc59a816f1ecc7642a60041a2f18cc
= <=>
2024-03-25 09:58:25 +0100

    m3-e3

---
+++ spring/src/main/java/com/example/spring/car/api/CarRepo.java
@@ -0,0 +1,10 @@
+package com.example.spring.car.api;
+
+import com.example.spring.car.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface CarRepo extends JpaRepository<Car, Integer> {
+    @Query(value = "select * from Car where id = ?", nativeQuery = true)
+    Car getCarById(Integer id);
+}
