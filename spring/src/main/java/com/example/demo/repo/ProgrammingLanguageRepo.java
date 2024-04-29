5f29cf2b934f4b019786bd0d9dd8d030e9edd0ff
BananaPilot <fabiopetrosillo2@gmail.com>
2024-04-29 10:53:58 +0200

    finito e5

--- spring/src/main/java/com/example/demo/repo/ProgrammingLanguageRepo.java
+++ spring/src/main/java/com/example/demo/repo/ProgrammingLanguageRepo.java
@@ -2,6 +2,6 @@ package com.example.demo.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CarRepo extends JpaRepository<Car, Long> {
+public interface ProgrammingLanguageRepo extends JpaRepository<ProgrammingLanguage, Long> {
 
 }
