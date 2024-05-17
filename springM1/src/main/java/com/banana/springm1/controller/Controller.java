01de88bc29a816f66f2cc2afb1aa4619fc81afe9
BananaPilot <fabiopetrosillo2@gmail.com>
2024-05-17 12:25:53 +0200

    ...

--- springM1/src/main/java/com/banana/springm1/controller/Controller.java
+++ springM1/src/main/java/com/banana/springm1/controller/Controller.java
@@ -1,10 +1,14 @@
 package com.banana.springm1.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalTime;
 
 @RestController
 public class Controller {
@@ -12,8 +16,23 @@ public class Controller {
     @Autowired
     private Environment env;
 
-    @GetMapping("/{name}")
-    public String devName(@PathVariable String name) {
-        return name + " " + env.getProperty("authCode") + " " + env.getProperty("devName");
+    Logger logger = LoggerFactory.getLogger(Controller.class);
+
+    @GetMapping("/")
+    public String devName() {
+        String toReturn = "Hello";
+        logger.debug("devName Controller response: {}", toReturn);
+        return toReturn;
+    }
+
+    @GetMapping("/exp")
+    public Double exponetial() {
+        StringBuilder stringBuilder = new StringBuilder();
+        Integer val1 = Integer.parseInt(env.getProperty("val1"));
+        Integer val2 = Integer.parseInt(env.getProperty("val2"));
+        logger.trace("time start val1: {}", LocalTime.now());
+        Double toReturn = Math.pow(val1, val2);
+        logger.trace("end time {}", LocalTime.now());
+        return toReturn;
     }
 }
